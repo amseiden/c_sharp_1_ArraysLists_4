b48c630a1ab3122c6ba380e4f055bdb8d0c5f82d
Ciornei Daniel <amseiden.mule@gmail.com>
2023-10-25 18:06:30 +0300

    Finished task successfully!

--- Program.cs
+++ Program.cs
@@ -1,11 +1,40 @@
-﻿namespace _1_ArraysLists_4
+﻿
+/*Write a program and ask the user to continuously enter a number or type "Quit" to exit.
+ The list of numbers may include duplicates. 
+ Display the unique numbers that the user has entered.*/
+
+namespace _1_ArraysLists_4
 {
     class Program
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("User enter a number: ");
+            List<int> numbers = new List<int>();
+            bool ruleaza = true;
 
+            while (ruleaza)
+            {
+                Console.WriteLine("Enter a number or \"quit\": ");
+                string userInput = Console.ReadLine();
+                if (userInput == "quit")
+                    ruleaza = false;
+                else
+                {
+                    int.TryParse(userInput, out int myNumber);
+                    numbers.Add(myNumber);
+                }
+            }
+
+            List<int> distinctNumbers = new List<int>();
+            foreach (int number in numbers)
+            {
+                if (!distinctNumbers.Contains(number))
+                {
+                    distinctNumbers.Add(number);
+                }
+            }
+
+            Console.WriteLine("\nYou previously entered: "+string.Join(", ", distinctNumbers));
         }
     }
 }
