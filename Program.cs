fca12a8517ee396d4f79e33cb4c515d37943fd0c
Ciornei Daniel <amseiden.mule@gmail.com>
2023-10-25 17:11:57 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace _1_ArraysLists_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("User enter a number: ");
+
+        }
+    }
+}
+
+
